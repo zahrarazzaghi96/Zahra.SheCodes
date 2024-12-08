588e3d5ff333325ed065066a5a5313fb3ab498e7
zahrarazzaghi96 <zahra.razzaghi96@gmail.com>
2024-12-08 09:44:04 +0330

    week6-Lesson1-challenge1

---
+++ src/index.js
@@ -0,0 +1,50 @@
+function displayTemp(response) {
+    let tempElement = document.querySelector("#temp");
+    tempElement.innerHTML = Math.round(response.data.temperature.current);
+    let cityElement = document.querySelector("#current-city");
+    cityElement.innerHTML = response.data.city;
+  }
+  
+  function search(event) {
+    event.preventDefault();
+    let searchInputElement = document.querySelector("#search-input");
+    let city = searchInputElement.value;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=483abfb4bt0oa2508177f7baa6732b73&units=metric`;
+  
+    axios.get(apiUrl).then(displayTemp);
+  }
+  
+  function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let day = date.getDay();
+  
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+  
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+  
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+  
+    let formattedDay = days[day];
+    return `${formattedDay} ${hours}:${minutes}`;
+  }
+  
+  let searchForm = document.querySelector("#search-form");
+  searchForm.addEventListener("submit", search);
+  
+  let currentDateELement = document.querySelector("#current-date");
+  let currentDate = new Date();
+  
+  currentDateELement.innerHTML = formatDate(currentDate);
